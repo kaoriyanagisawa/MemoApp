be4073339a7b750ba6c7ff3d710f0c42b3576ccb
yanagisawa <46134277+kaoriyanagisawa@users.noreply.github.com>
2024-07-21 19:47:08 +0900

    Circle buttom style

--- Dev/Udemy/MemoApp/src/components/Hello.tsx
+++ Dev/Udemy/MemoApp/src/components/Hello.tsx
@@ -7,21 +7,21 @@ interface Props {
 }
 
 
-const Hello = (props:Props): JSX.Element => {
+const Hello = (props: Props): JSX.Element => {
     const { children, bang, style } = props
-    return(
+    return (
         <View>
-            <Text style={[styles.text,style]}>
+            <Text style={[styles.text, style]}>
                 Hello {children}{bang === true ? '!' : ''}
-                </Text>
+            </Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    text:{
-        color:'#ffffff',
-        backgroundColor:'blue',
+    text: {
+        color: '#ffffff',
+        backgroundColor: 'blue',
         fontSize: 40,
         fontWeight: 'bold',
         padding: 16
