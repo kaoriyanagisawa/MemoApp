e6bc714ca26ed62957028cd30db8cd21d12810de
yanagisawa <46134277+kaoriyanagisawa@users.noreply.github.com>
2024-07-21 23:56:34 +0900

    Memo detail markup

--- Dev/Udemy/MemoApp/src/components/Header.tsx
+++ Dev/Udemy/MemoApp/src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header = (): JSX.Element => {
     return (
         <View style={styles.header}>
             <View style={styles.headerInner}>
-                <Text style={styles.headerTitle}>Meom App</Text>
+                <Text style={styles.headerTitle}>Memo App</Text>
                 <Text style={styles.headerRight}>ログアウト</Text>
             </View>
         </View>
