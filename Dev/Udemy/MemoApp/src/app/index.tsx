51fec6bc64d7937ac02b5c6805ef3e14daed74f3
yanagisawa <46134277+kaoriyanagisawa@users.noreply.github.com>
2024-07-21 17:41:27 +0900

    MemoListItem styl

--- Dev/Udemy/MemoApp/src/app/index.tsx
+++ Dev/Udemy/MemoApp/src/app/index.tsx
@@ -12,10 +12,11 @@ const Index = () => {
             </View>
 
             <View>
-                <View>
+
+                <View style={styles.memoListItem}>
                     <View>
-                        <Text>買い物リスト</Text>
-                        <Text>2023年10月1日　10:00</Text>
+                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+                        <Text style={styles.memoListItemDate}>2023年10月1日　10:00</Text>
                     </View>
                     <View>
                         <Text>×</Text>
@@ -23,20 +24,20 @@ const Index = () => {
                 </View>
 
 
-                <View>
+                <View style={styles.memoListItem}>
                     <View>
-                        <Text>買い物リスト</Text>
-                        <Text>2023年10月1日　10:00</Text>
+                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+                        <Text style={styles.memoListItemDate}>2023年10月1日　10:00</Text>
                     </View>
                     <View>
                         <Text>×</Text>
                     </View>
                 </View>
 
-                <View>
+                <View style={styles.memoListItem}>
                     <View>
-                        <Text>買い物リスト</Text>
-                        <Text>2023年10月1日　10:00</Text>
+                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+                        <Text style={styles.memoListItemDate}>2023年10月1日　10:00</Text>
                     </View>
                     <View>
                         <Text>×</Text>
@@ -77,7 +78,29 @@ const styles = StyleSheet.create({
         fontSize: 32,
         fontWeight: 'bold',
         color: '#ffffff'
+    },
+    memoListItem: {
+        backgroundColor: '#ffffff',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 16,
+        paddingHorizontal: 19,
+        alignItems: 'center',
+        borderBottomWidth: 1,
+        borderColor: 'rgba(0,0,0,0.15)'
+    },
+    memoListItemTitle: {
+        fontSize: 16,
+        lineHeight: 32
+    },
+    memoListItemDate: {
+        fontSize: 12,
+        lineHeight: 16,
+        color: '#848484'
     }
+
+
+
 
 })
 
