32e03c7706f9991b40b0c81b87bd20dff2f7affb
yanagisawa <46134277+kaoriyanagisawa@users.noreply.github.com>
2024-07-29 02:23:31 +0900

    delete memo and firestore protect rule Please enter the commit message for your changes. Lines starting

--- Dev/Udemy/MemoApp/src/components/LogOutButton.tsx
+++ Dev/Udemy/MemoApp/src/components/LogOutButton.tsx
@@ -1,8 +1,24 @@
-import { Text, StyleSheet, TouchableOpacity,  } from "react-native"
+import { Text, StyleSheet, TouchableOpacity, Alert,  } from "react-native"
+import { signOut } from "firebase/auth"
+import { router } from "expo-router"
+
+import { auth } from "../config"
+
+const handlePress = ():void => {
+    signOut(auth)
+    .then(() => {
+        router.replace('/auth/log_in')
+    })
+    .catch(
+        () =>(
+            Alert.alert('ログアウトに失敗しました')
+    ))
+}
+
 
 const LogOutBottun = ():JSX.Element => {
     return(
-        <TouchableOpacity>
+        <TouchableOpacity onPress={handlePress}>
             <Text style={styles.text}>ログアウト</Text>
         </TouchableOpacity>
     )
